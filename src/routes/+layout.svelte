f76c56e5fa6d38fc3239493f3c4221c51ed0bc62
lowkey <karolis.laukys@tesonet.com>
2023-12-15 22:13:27 +0200

    feat: created initial layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,49 @@
-<script>
+<script lang="ts">
 	import '@fontsource/inter';
 	import '../styles/styles.css';
+
+	import { Header } from '$lib';
 </script>
 
-<slot />
+<div class="layout-wrapper">
+	<Header
+		links={[
+			{ label: 'home', link: '/' },
+			{ label: 'new news. on culture.', link: '/' },
+			{ label: 'style > fashion', link: '/' },
+			{ label: `let's talk about food`, link: '/' },
+			{ label: 'hip hop. eternal source of energy', link: '/' },
+			{ label: 'anything goes', link: '/' }
+		]}
+	/>
+	<div class="page-content-wrapper">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.layout-wrapper {
+		max-width: 1512px;
+		margin: 32px auto;
+
+		padding: 0 24px;
+	}
+
+	@media (min-width: 700px) {
+		.layout-wrapper {
+			padding: 0 32px;
+		}
+	}
+
+	.page-content-wrapper {
+		display: flex;
+		flex-direction: column;
+		row-gap: 24px;
+	}
+
+	@media (min-width: 1460px) {
+		.page-content-wrapper {
+			row-gap: 40px;
+		}
+	}
+</style>
