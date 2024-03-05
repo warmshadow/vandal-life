f29c538f3f4d891316a422e20c7b930892a366b1
lowkey <karolis.laukys@tesonet.com>
2024-03-05 22:30:37 +0200

    feat: categorySlug page added condition

--- src/routes/[categorySlug]/+page.svelte
+++ src/routes/[categorySlug]/+page.svelte
@@ -7,15 +7,14 @@
 	export let data;
 </script>
 
-<CardBigText
-	content={[{ component: 'bigText', leftText: data.categoryStories.name }]}
-	isCategoryName
-/>
-<ArticlesSection
-	data={data.categoryStories.data.stories.map((story) => ({
-		src: placeholder,
-		title: story.content.title,
-		description: story.content.subtitle,
-		link: { to: `/${story.full_slug}` }
-	}))}
-/>
+{#if data.categoryStories}
+	<CardBigText content={[{ component: 'bigText', leftText: data.categoryStories.name }]} />
+	<ArticlesSection
+		data={data.categoryStories.data.stories.map((story) => ({
+			src: placeholder,
+			title: story.content.title,
+			description: story.content.subtitle,
+			link: { to: `/${story.full_slug}` }
+		}))}
+	/>
+{/if}
