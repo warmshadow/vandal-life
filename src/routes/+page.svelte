8ee35210547d09cbd933d587e651bf2bc83169fd
lowkey <karolis.laukys@tesonet.com>
2024-04-06 22:44:42 +0300

    feat: editable categoryName blok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { type ComponentProps, onMount } from 'svelte';
 	import { useStoryblokBridge, StoryblokComponent } from '@storyblok/svelte';
+	import { storyblokEditable } from '@storyblok/svelte';
 
 	import CategorySection from '$lib/sections/CategorySection.svelte';
 	import Modal from '$lib/Modal/Modal.svelte';
@@ -29,69 +30,71 @@
 		(category) => category.data.stories[0].content.component !== 'idea'
 	)}
 
-	{#each data.story.content.listOfBlocks as block}
-		{#if block.component === 'messageCard'}
-			<StoryblokComponent blok={block} />
+	{#each data.story.content.listOfBlocks as blok}
+		{#if blok.component === 'messageCard'}
+			<StoryblokComponent {blok} />
 		{/if}
 
-		{#if block.component === 'categoryName'}
-			{@const category = data.categoriesStories.find((category) => category.slug === block.slug)}
-			{#if category}
-				{#if category.data.stories[0].content.component !== 'idea'}
-					{@const index = allPostCategories.findIndex((category) => category.slug === block.slug)}
+		{#if blok.component === 'categoryName'}
+			{@const category = data.categoriesStories.find((category) => category.slug === blok.slug)}
+			<div use:storyblokEditable={blok}>
+				{#if category}
+					{#if category.data.stories[0].content.component !== 'idea'}
+						{@const index = allPostCategories.findIndex((category) => category.slug === blok.slug)}
 
-					<!-- post stories -->
-					<CategorySection
-						data={{
-							categoryCard: {
-								content: [
-									{
-										component: 'bigText',
-										leftText: block.title
+						<!-- post stories -->
+						<CategorySection
+							data={{
+								categoryCard: {
+									content: [
+										{
+											component: 'bigText',
+											leftText: blok.title
+										}
+									]
+								},
+								// slicing first three posts
+								cards: category.data.stories.slice(0, 3).map((story) => ({
+									// @TODO handle without this placeholder
+									src: optimizeImage(story.content.featuredImage, '1068x392') ?? placeholder,
+									title: story.content.title,
+									description: story.content.subtitle,
+									link: { to: `/${story.full_slug}` }
+								})),
+								categoryLink: `/${category.slug}`
+							}}
+							altOrder={!(index % 2)}
+						/>
+					{:else}
+						<!-- idea story -->
+						<CategorySection
+							data={{
+								categoryCard: {
+									content: [
+										{
+											component: 'bigText',
+											leftText: blok.title
+										}
+									]
+								},
+								// slicing first 3 ads
+								cards: category.data.stories[0].content.ads.slice(0, 3).map((ad) => ({
+									title: ad.title,
+									link: {
+										label: 'Explore the idea',
+										onClick: () => {
+											modalContent = { title: ad.title, description: ad.content };
+											showModal = true;
+										}
 									}
-								]
-							},
-							// slicing first three posts
-							cards: category.data.stories.slice(0, 3).map((story) => ({
-								// @TODO handle without this placeholder
-								src: optimizeImage(story.content.featuredImage, '1068x392') ?? placeholder,
-								title: story.content.title,
-								description: story.content.subtitle,
-								link: { to: `/${story.full_slug}` }
-							})),
-							categoryLink: `/${category.slug}`
-						}}
-						altOrder={!(index % 2)}
-					/>
-				{:else}
-					<!-- idea story -->
-					<CategorySection
-						data={{
-							categoryCard: {
-								content: [
-									{
-										component: 'bigText',
-										leftText: block.title
-									}
-								]
-							},
-							// slicing first 3 ads
-							cards: category.data.stories[0].content.ads.slice(0, 3).map((ad) => ({
-								title: ad.title,
-								link: {
-									label: 'Explore the idea',
-									onClick: () => {
-										modalContent = { title: ad.title, description: ad.content };
-										showModal = true;
-									}
-								}
-							})),
-							categoryLink: `/${category.slug}`
-						}}
-						altOrder={false}
-					/>
+								})),
+								categoryLink: `/${category.slug}`
+							}}
+							altOrder={false}
+						/>
+					{/if}
 				{/if}
-			{/if}
+			</div>
 		{/if}
 	{/each}
 {/if}
